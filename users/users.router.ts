462937af0118d813dfc18d1399ae1fe55ec6fd30
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-11-20 17:56:09 -0300

    Feat - Adding a more concisve response to user route

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,11 +1,15 @@
 import { Router } from '../common/router'
 import * as restify from 'restify'
+import { User } from './users.model'
 
 class UsersRouter extends Router {
 
     applyRoutes(application: restify.Server ) {
         application.get('/users', (req, resp, next) => {
-            resp.json({ message: 'users router'})
+            User.findAll().then(users => {
+                resp.json(users)
+                return next()
+            })
         })
     }
 
