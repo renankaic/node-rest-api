cc496d49b7807fded6660793a3521429febe2b15
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-11-25 14:31:16 -0300

    Ref - UsersRouter - Implementing render() method to endpoints. Adding 'beforeRender' listening to remove user password from the object before sending it to requester

--- users/users.router.ts
+++ users/users.router.ts
@@ -4,39 +4,31 @@ import { User } from './users.model'
 
 class UsersRouter extends Router {
 
+    constructor() {
+        super()
+        this.on('beforeRender', document => {
+            document.password = undefined
+        })
+    }
+
     applyRoutes(application: restify.Server ) {
         application.get('/users', (req, resp, next) => {
             User
                 .find()
-                .then(users => {
-                    resp.json(users)
-                    return next()
-                })
+                .then(this.render(resp, next))
         })
 
         application.get('/users/:id', (req, resp, next) => {
             User
                 .findById(req.params.id)
-                .then(user => {
-                    if (user) {
-                        resp.json(user)
-                        return next()
-                    }
-
-                    resp.send(404)
-                    return next()
-                })
+                .then(this.render(resp, next))
         })
 
         application.post('/users', (req, resp, next) => {
             let user = new User(req.body)
             user
                 .save()
-                .then(user => {
-                    user.password = undefined
-                    resp.json(user)
-                    return next()
-                })
+                .then(this.render(resp, next))
         })
 
         application.put('/users/:id', (req, resp, next) => {
@@ -52,24 +44,14 @@ class UsersRouter extends Router {
                         resp.send(404)
                     }
                 })
-                .then(user => {
-                    resp.json(user)
-                    return next()
-                })
+                .then(this.render(resp, next))
         })
 
         application.patch('/users/:id', (req, resp, next) => {
             const options = { new: true }
             User
                 .findByIdAndUpdate(req.params.id, req.body, options)
-                .then(user => {
-                    if(user) {
-                        resp.json(user)
-                        return next()
-                    } 
-                    resp.send(404)
-                    return next()
-                })
+                .then(this.render(resp, next))
         })
 
         application.del('/users/:id', (req, resp, next) => {
