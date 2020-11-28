c3aff890917ebcda69b4d8b358ad32eef1594108
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-11-28 12:41:30 -0300

    Feat - Error handler - Customizing validation error

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -16,7 +16,13 @@ export const handleError = (req: restify.Request, resp: restify.Response, err, d
             break;
         case "ValidationError":
             err.statusCode = 400
-    
+            const messages: any[] = []
+            for(let name in err.errors) {
+                messages.push({ message: err.errors[name].message })
+            }
+            err.toJSON = () => ({
+                errors: messages
+            })
         default:
             break;
     }
