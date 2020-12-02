268b46309a89da7d14e97c8cfff7772df26906ec
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-01 23:28:40 -0300

    Ref - Creating generic methods in model-router to be used in their inherited classes.
    Applying refactoring in users.routers to the new model-router methods

--- common/model-router.ts
+++ common/model-router.ts
@@ -1,5 +1,6 @@
 import { Router } from './router'
 import * as mongoose from 'mongoose'
+import { NotFoundError } from 'restify-errors'
 
 export abstract class ModelRouter<D extends mongoose.Document> extends Router {
 
@@ -11,6 +12,67 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
         this.model
             .find()
             .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    findById = (req, resp, next) => {
+        this.model
+            .findById(req.params.id)
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    save = (req, resp, next) => {
+        let document = new this.model(req.body)
+        document
+            .save()
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    replace = (req, resp, next) => {
+        const options = {
+            overwrite: true,
+            runValidators: true
+        };
+
+        (<any>this.model)
+            .update({ _id: req.params.id }, req.body, options)
+            .exec()
+            .then(result => {
+                if (result.n) {
+                    return this.model.findById(req.params.id)
+                } else {
+                    throw new NotFoundError('Usuário não encontrado')
+                }
+            })
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    update = (req, resp, next) => {
+        const options = {
+            new: true,
+            runValidators: true
+        }
+        this.model
+            .findByIdAndUpdate(req.params.id, req.body, options)
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    delete = (req, resp, next) => {
+        this.model
+            .remove({ _id: req.params.id })
+            .exec()
+            .then((commandResult: any) => {
+                if (commandResult.result.n) {
+                    resp.send(204)
+                } else {
+                    throw new NotFoundError('Documento não encontrado')
+                }
+                return next()
+            })
             .catch(next)
     }
 
