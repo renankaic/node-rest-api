9a189630d7361c4e4b307308076bd90156f2c4c2
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-03 13:50:09 -0300

    Feat - Reviews - Router and some fixes

---
+++ reviews/reviews.router.ts
@@ -0,0 +1,21 @@
+import * as restify from 'restify'
+import * as mongoose from 'mongoose'
+import { ModelRouter } from '../common/model-router'
+import { Review } from './reviews.model'
+
+class ReviewsRouter extends ModelRouter<Review>{
+    
+    constructor() {
+        super(Review)
+    }
+
+    applyRoutes(application: restify.Server) {
+        application.get('/reviews', this.findAll)
+        application.get('/reviews/:id', [this.validateId, this.findById])
+        application.post('/reviews', this.save)
+        application.del('/reviews/:id', [this.validateId, this.delete])
+    }
+}
+
+export const reviewsRouter = new ReviewsRouter()
+
