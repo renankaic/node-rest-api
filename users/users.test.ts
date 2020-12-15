d9c0ca89e694948b92b8b7b38e7dcf5744d089e5
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-15 20:32:52 -0300

    Feat - Tests -Testing POST

--- users/users.test.ts
+++ users/users.test.ts
@@ -10,3 +10,23 @@ test('get /users', () => {
         })
         .catch(fail)
 })
+
+test('post /users', () => {
+    return request('http://localhost:3000')
+        .post('/users')
+        .send({
+            name: 'usuario1',
+            email: 'usuario1@email.com',
+            password: '123456',
+            cpf: '597.719.810-80'
+        })
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('usuario1')
+            expect(response.body.email).toBe('usuario1@email.com')
+            expect(response.body.cpf).toBe('597.719.810-80')
+            expect(response.body.password).toBeUndefined()
+        })
+        .catch(fail)
+})
