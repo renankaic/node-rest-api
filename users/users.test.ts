2f3c60b2dd8f23c98cbe9bfda034d341f81343a8
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-15 20:26:41 -0300

    Feat - Tests - Creating first test for Users with jest and supertest

--- users/users.test.ts
+++ users/users.test.ts
@@ -1,5 +1,12 @@
 import "jest"
+import * as request from 'supertest'
 
-test('Primeiro teste', () => {
-    
+test('get /users', () => {
+    return request('http://localhost:3000')
+        .get('/users')
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body.items).toBeInstanceOf(Array)
+        })
+        .catch(fail)
 })
