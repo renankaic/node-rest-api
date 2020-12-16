17b05f2f72081abcd6c72300d192c1dc2ab1e2c0
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-16 09:45:49 -0300

    Feat - Tests - Testing Patch endpoint

--- users/users.test.ts
+++ users/users.test.ts
@@ -59,6 +59,31 @@ test('get /users/aaaaa - not found', () => {
         .catch(fail)
 })
 
+test('patch /users/:id', () => {
+    return request(address)
+        .post('/users')
+        .send({
+            name: 'usuario2',
+            email: 'usuario2@email.com',
+            password: '123456'
+        })
+        .then(response => {
+            return request(address)
+                    .patch(`/users/${response.body._id}`)
+                    .send({
+                        name: 'usuario2 - patch'
+                    })
+        })
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('usuario2 - patch')
+            expect(response.body.email).toBe('usuario2@email.com')
+            expect(response.body.password).toBeUndefined()
+        })
+        .catch(fail)
+})
+
 afterAll(() => {
     return server.shutdown()
 })
