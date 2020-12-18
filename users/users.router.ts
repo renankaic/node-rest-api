72050fe2d2cf1bc3910191ec9bbc8e826b82e8a4
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-18 17:00:25 -0300

    Feat - Auth - Creating authentication route on user.router

--- users/users.router.ts
+++ users/users.router.ts
@@ -2,6 +2,7 @@ import { ModelRouter } from '../common/model-router'
 import * as restify from 'restify'
 import { User } from './users.model'
 import { NotFoundError } from 'restify-errors'
+import { authenticate } from '../security/auth.handler'
 
 class UsersRouter extends ModelRouter<User> {
 
@@ -38,6 +39,8 @@ class UsersRouter extends ModelRouter<User> {
         application.put(`${this.basePath}/:id`, [this.validateId, this.replace])
         application.patch(`${this.basePath}/:id`, [this.validateId, this.update])
         application.del(`${this.basePath}/:id`, [this.validateId, this.delete])
+
+        application.post(`${this.basePath}/authenticate`, authenticate)
 
     }
 
