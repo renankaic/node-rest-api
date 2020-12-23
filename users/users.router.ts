ee9a9635530fc1866ac6851b945ad3ee108dcccc
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-12-23 19:44:16 -0300

    Feat - Auth - Protecting user router with 'authorize' handler

--- users/users.router.ts
+++ users/users.router.ts
@@ -3,6 +3,7 @@ import * as restify from 'restify'
 import { User } from './users.model'
 import { NotFoundError } from 'restify-errors'
 import { authenticate } from '../security/auth.handler'
+import { authorize } from '../security/authz.handler'
 
 class UsersRouter extends ModelRouter<User> {
 
@@ -32,13 +33,45 @@ class UsersRouter extends ModelRouter<User> {
 
         //Specify the version by using 'accept-version' header
 
-        application.get({ path: `${this.basePath}`, version: '2.0.0' }, [this.findByEmail, this.findAll])
-        application.get({ path: `${this.basePath}`, version: '1.0.0' }, this.findAll)
-        application.get(`${this.basePath}/:id`,[this.validateId, this.findById])
-        application.post(`${this.basePath}`, this.save)
-        application.put(`${this.basePath}/:id`, [this.validateId, this.replace])
-        application.patch(`${this.basePath}/:id`, [this.validateId, this.update])
-        application.del(`${this.basePath}/:id`, [this.validateId, this.delete])
+        application.get({ path: `${this.basePath}`, version: '2.0.0' }, [
+            authorize('admin'),
+            this.findByEmail, 
+            this.findAll
+        ])
+
+        application.get({ path: `${this.basePath}`, version: '1.0.0' }, [
+            authorize('admin'),
+            this.findAll
+        ])
+
+        application.get(`${this.basePath}/:id`,[
+            authorize('admin'),
+            this.validateId, 
+            this.findById
+        ])
+        
+        application.post(`${this.basePath}`, [
+            authorize('admin'), 
+            this.save
+        ])
+
+        application.put(`${this.basePath}/:id`, [
+            authorize('admin'),
+            this.validateId, 
+            this.replace
+        ])
+
+        application.patch(`${this.basePath}/:id`, [
+            authorize('admin'),
+            this.validateId, 
+            this.update
+        ])
+
+        application.del(`${this.basePath}/:id`, [
+            authorize('admin'),
+            this.validateId, 
+            this.delete
+        ])
 
         application.post(`${this.basePath}/authenticate`, authenticate)
 
