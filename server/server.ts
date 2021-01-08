6cb92cb369649f92bcf02b32b561b3457cf91d4f
Renan Kaic Lopes <renan-kaic@outlook.com>
2021-01-07 22:36:54 -0300

    Feat - Security - Setting HTTPS with self signed certificates

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
+import * as fs from 'fs'
 import { environment } from '../common/environment'
 import { Router } from '../common/router'
 import { mergePatchBodyParser } from './merge-patch.parser'
@@ -24,7 +25,9 @@ export class Server {
                 
                 this.application = restify.createServer({
                     name: 'meat-api',
-                    version: '1.0.0'
+                    version: '1.0.0',
+                    certificate: fs.readFileSync('./security/keys/cert.pem'),
+                    key: fs.readFileSync('./security/keys/key.pem')
                 })
 
                 this.application.use(restify.plugins.queryParser())
